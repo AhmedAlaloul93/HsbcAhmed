d890f1a73541855f100279488c102330620a3f5a
Ahmed Alaloul <ahmedalaloul@icloud.com>
2019-11-18 04:02:24 +0200

    Initial Commit

---
+++ HsbcAhmed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HsbcAhmed
+//
+//  Created by Ahmed Alaloul on 11/18/19.
+//  Copyright © 2019 AhmedAlaloul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
